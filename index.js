84854112242a389383e411ff3e6493af02669b1c
PATHAN SALMAN KHAN <54095769+salmankhan-prs@users.noreply.github.com>
2023-12-16 17:28:32 +0530

    feat: added basic version 2

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const llm = new ChatOpenAI({ openAIApiKey });
 // Templates for standalone questions and answers
 const standAloneQuestionTemplate = `Given a conversation history (if any) and a question, convert it to a standalone question.\nConversation history: {conv_history}\nQuestion: {question} Standalone question:`;
 
-const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided and the conversation history. Try to find the answer in the context. If the answer is not given in the context, find the answer in the conversation history if possible. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@company.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.\nContext: {context}\nConversation history: {conv_history}\nQuestion: {question}\nAnswer: `;
+const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about company  based on the context provided and the conversation history. Try to find the answer in the context. If the answer is not given in the context, find the answer in the conversation history if possible. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@company.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.\nContext: {context}\nConversation history: {conv_history}\nQuestion: {question}\nAnswer: `;
 
 // Initialize prompt templates and chains
 const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
@@ -83,14 +83,14 @@ app.post('/api/chat', async (req, res) => {
 
     // Get or initialize conversation history for the user
     const convHistory = userConvHistories[userId] || [];
-
+console.log({userId})
     // Append the new question to the conversation history
     convHistory.push(`Human: ${question}`);
 
     // Process the question and generate a response
     const response = await chain.invoke({
       question,
-      conv_history: formatConvHistory(convHistory),
+     // conv_history: formatConvHistory(convHistory),
     });
 
     // Append the AI response to the conversation history
